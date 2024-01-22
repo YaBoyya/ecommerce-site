d54f2b4ac3f4fa7556b111aaf8271441fc7eaf65
YaBoyya <duzniak.amadeusz@gmail.com>
2024-01-22 21:36:31 +0100

    Fix product model relationship and modify model modified_at field

--- core/models.py
+++ core/models.py
@@ -8,13 +8,13 @@ class Product(models.Model):
     SKU = models.CharField(max_length=10)
     category_id = models.ForeignKey('core.ProductCategory', null=True,
                                     on_delete=models.SET_NULL)
-    inventory_id = models.ForeignKey('core.ProductInventory', null=True,
-                                     on_delete=models.SET_NULL)
+    inventory_id = models.OneToOneField('core.ProductInventory', null=True,
+                                        on_delete=models.CASCADE)
     price = models.FloatField(_('Price'))
     discount_id = models.ForeignKey('core.Discount', null=True,
                                     on_delete=models.SET_NULL)
     created_at = models.DateTimeField(_('Created at'), auto_now_add=True)
-    modified_at = models.DateTimeField(_('Modified at'), auto_now_add=True)
+    modified_at = models.DateTimeField(_('Modified at'), null=True)
     deleted_at = models.DateTimeField(_('Deleted at'), default=None, null=True)
 
     class Meta:
@@ -25,7 +25,7 @@ class ProductCategory(models.Model):
     name = models.CharField(_('Name'), max_length=150)
     desc = models.TextField(_('Description'), max_length=500)
     created_at = models.DateTimeField(_('Created at'), auto_now_add=True)
-    modified_at = models.DateTimeField(_('Modified at'), auto_now_add=True)
+    modified_at = models.DateTimeField(_('Modified at'), null=True)
     deleted_at = models.DateTimeField(_('Deleted at'), default=None, null=True)
 
     class Meta:
@@ -35,7 +35,7 @@ class ProductCategory(models.Model):
 class ProductInventory(models.Model):
     quantity = models.IntegerField(_('Quantity'), default=0)
     created_at = models.DateTimeField(_('Created at'), auto_now_add=True)
-    modified_at = models.DateTimeField(_('Modified at'), auto_now_add=True)
+    modified_at = models.DateTimeField(_('Modified at'), null=True)
     deleted_at = models.DateTimeField(_('Deleted at'), default=None, null=True)
 
     class Meta:
@@ -48,7 +48,7 @@ class Discount(models.Model):
     discount_percent = models.DecimalField(max_digits=3, decimal_places=0)
     active = models.BooleanField(default=False)
     created_at = models.DateTimeField(_('Created at'), auto_now_add=True)
-    modified_at = models.DateTimeField(_('Modified at'), auto_now_add=True)
+    modified_at = models.DateTimeField(_('Modified at'), null=True)
     deleted_at = models.DateTimeField(_('Deleted at'), default=None, null=True)
 
     class Meta:

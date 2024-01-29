22abc281dfc0f3222c0d1d84a6d4a47c732c278e
YaBoyya <duzniak.amadeusz@gmail.com>
2024-01-29 10:06:08 +0100

    Set up initial serializers

---
+++ users/serializers.py
@@ -0,0 +1,44 @@
+from django.contrib.auth import authenticate
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework import serializers
+
+from users.models import ECommerceUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ECommerceUser
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 6}}
+
+    def create(self, validated_data):
+        return ECommerceUser.objects.create_user(**validated_data)
+
+
+class AuthSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_style': 'password'},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            user = authenticate(
+                request=self.context.get('request'),
+                email=email,
+                password=password
+            )
+            if not user:
+                msg = _('Unable to log in with provided credentials')
+                raise serializers.ValidationError(msg, code='authentication')
+        else:
+            msg = _('Must include "username" and "password".')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs

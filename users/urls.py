760f4fdd685cfc7e8263eec2ed402908a6816428
YaBoyya <duzniak.amadeusz@gmail.com>
2024-01-29 12:36:15 +0100

    Create user related views and modify urls

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,15 @@
-from rest_framework.routers import DefaultRouter
+from django.urls import path
+
+from knox import views as knox_views
 
 from . import views
 
 
 app_name = 'users'
-router = DefaultRouter()
-
-
-urlpatterns = router.urls
+urlpatterns = [
+    path('create', views.CreateUserView.as_view(), name="create"),
+    path('profile', views.ManageUserView.as_view(), name='profile'),
+    path('login', views.LoginView.as_view(), name='knox_login'),
+    path('logout', knox_views.LogoutView.as_view(), name='knox_logout'),
+    path('logoutall', knox_views.LogoutAllView.as_view(), name='knox_logoutall'),  # noqa
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth import login
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+from knox.views import LoginView as KnoxLoginView
+
+from users.serializers import UserSerializer, AuthSerializer
+
+
+# TODO setup a viewset for this
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class LoginView(KnoxLoginView):
+    serializer_class = AuthSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, format=None):
+        serializer = AuthSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginView, self).post(request, format=None)
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user

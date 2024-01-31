3f03eb9c770b85811c8e40233413f000e35f44df
YaBoyya <duzniak.amadeusz@gmail.com>
2024-01-31 12:58:12 +0100

    Add shopping related models

--- shopping/models.py
+++ shopping/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from core.models import BaseECommerceModel
+
+
+class ShoppingSession(BaseECommerceModel):
+    user = models.OneToOneField('users.ECommerceUser',
+                                on_delete=models.CASCADE)
+    total = models.DecimalField(_('total'), max_digits=5, decimal_places=2)
+
+
+class CartItem(BaseECommerceModel):
+    session = models.ForeignKey(ShoppingSession,
+                                related_name='cart_item',
+                                on_delete=models.CASCADE)
+    product = models.OneToOneField('core.Product', on_delete=models.CASCADE)
+    quantity = models.IntegerField(_('quantity'), default=1)

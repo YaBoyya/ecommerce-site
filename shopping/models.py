75b7464b86c6f1ac7b9dad97c53268ec78adf05a
YaBoyya <duzniak.amadeusz@gmail.com>
2024-02-03 17:43:22 +0100

    Change Product-CartItem relationchip to one to many

--- shopping/models.py
+++ shopping/models.py
@@ -6,13 +6,15 @@ from core.models import BaseECommerceModel
 
 class ShoppingSession(BaseECommerceModel):
     user = models.OneToOneField('users.ECommerceUser',
-                                on_delete=models.CASCADE)
-    total = models.DecimalField(_('total'), max_digits=5, decimal_places=2)
+                                on_delete=models.CASCADE,
+                                null=True, blank=True)
+    total = models.DecimalField(_('total'), max_digits=5, decimal_places=2,
+                                default=0)
 
 
 class CartItem(BaseECommerceModel):
     session = models.ForeignKey(ShoppingSession,
                                 related_name='cart_item',
                                 on_delete=models.CASCADE)
-    product = models.OneToOneField('core.Product', on_delete=models.CASCADE)
+    product = models.ForeignKey('core.Product', on_delete=models.CASCADE)
     quantity = models.IntegerField(_('quantity'), default=1)

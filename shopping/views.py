4d6e0055f8bcd3c7d7ebbac73a93ce3de45e29e4
YaBoyya <duzniak.amadeusz@gmail.com>
2024-02-07 16:37:08 +0100

    Add creating of orders

--- shopping/views.py
+++ shopping/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -11,12 +12,19 @@ class OrderDetailsView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get_object(self):
-        return OrderDetails.objects.get_or_create(user=self.request.user)
+        return OrderDetails.objects.filter(user=self.request.user)
 
+    # TODO decide what it should show either list or latest order
     def get(self, request, format=None):
-        session = self.get_object()[0]
-        serializer = OrderDetailsSerializer(session)
+        session = self.get_object()
+        serializer = OrderDetailsSerializer(session, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = OrderDetailsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 # TODO Maybe change the url to something like product/<str:pk>/add

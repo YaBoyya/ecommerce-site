9e36dac2d58b5eb5bed7bf9dec9529fc5529aaae
Amadeusz <duzniak.amadeusz@gmail.com>
2024-02-13 21:44:27 +0100

    Modify field name

--- shopping/serializers.py
+++ shopping/serializers.py
@@ -2,17 +2,17 @@ from django.db.models import Sum
 
 from rest_framework import serializers
 
-from shopping.models import OrderItems, OrderDetails
+from shopping.models import OrderItem, OrderDetails
 
 
-class OrderItemsSerializer(serializers.ModelSerializer):
+class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
-        model = OrderItems
+        model = OrderItem
         fields = '__all__'
 
 
 class OrderDetailsSerializer(serializers.ModelSerializer):
-    order_items = OrderItemsSerializer(many=True, read_only=True)
+    items = OrderItemSerializer(many=True, read_only=True)
     total = serializers.SerializerMethodField()
 
     class Meta:
@@ -20,5 +20,5 @@ class OrderDetailsSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def get_total(self, obj):
-        return OrderItems.objects.filter(order=obj)\
+        return OrderItem.objects.filter(order=obj)\
             .aggregate(total=Sum('product__price')).get('total')

67fba2575b37bdd980c1a271621a7e0fe95e2811
Amadeusz <duzniak.amadeusz@gmail.com>
2024-03-09 17:16:42 +0100

    Remove IsStaff permission as it was code duplicate

--- core/views.py
+++ core/views.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 from core import serializers
 from core.mixins import ProductSearchMixin, SearchMixin
 from core.models import Discount, Product, ProductCategory, ProductInventory
-from core.permissions import IsStaff
 
 
 class IndexViewSet(ProductSearchMixin,
@@ -16,24 +16,24 @@ class ProductManagementViewSet(ProductSearchMixin,
                                viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = serializers.ProductSerializer
-    permission_classes = [IsStaff]
+    permission_classes = [IsAdminUser]
 
 
 class ProductCategoryManagement(SearchMixin,
                                 viewsets.ModelViewSet):
     queryset = ProductCategory.objects.all()
     serializer_class = serializers.ProductCategorySerializer
-    permission_classes = [IsStaff]
+    permission_classes = [IsAdminUser]
 
 
 class ProductInventoryManagement(viewsets.ModelViewSet):
     queryset = ProductInventory.objects.all()
     serializer_class = serializers.ProductInventorySerializer
-    permission_classes = [IsStaff]
+    permission_classes = [IsAdminUser]
 
 
 class DiscountManagement(SearchMixin,
                          viewsets.ModelViewSet):
     queryset = Discount.objects.all()
     serializer_class = serializers.DiscountSerializer
-    permission_classes = [IsStaff]
+    permission_classes = [IsAdminUser]

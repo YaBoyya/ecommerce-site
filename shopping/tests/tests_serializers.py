79b538ea476952fcde5d45d6bee416ea143bd4ea
Amadeusz <duzniak.amadeusz@gmail.com>
2024-04-05 14:29:12 +0200

    Add CartItemSerializer tests

--- shopping/tests/tests_serializers.py
+++ shopping/tests/tests_serializers.py
@@ -3,7 +3,8 @@ from django.utils.translation import gettext_lazy as _
 
 from rest_framework.serializers import ValidationError
 
-from shopping.serializers import OrderDetailsSerializer
+from core.models import Product
+from shopping.serializers import CartItemSerializer, OrderDetailsSerializer
 
 
 class TestOrderDetailsSerializer(TestCase):
@@ -41,19 +42,47 @@ class TestOrderDetailsSerializer(TestCase):
         cancel_data = self.data.copy()
         cancel_data.update({'status': 'CANCELED'})
 
-        # print(self.data, cancel_data)
-
         serializer = OrderDetailsSerializer(data=cancel_data)
         serializer.is_valid(raise_exception=True)
         inst = serializer.save()
-        # print(inst.status, self.data['status'])
 
         serializer = OrderDetailsSerializer(data=self.data, instance=inst)
+
         with self.assertRaises(ValidationError):
             serializer.is_valid(raise_exception=True)
-        # serializer.save()
         self.assertIn(
             _('You cannot activate canceled order'),
             list(map(str, serializer.errors['status']))
         )
-        print(serializer.errors)
+
+
+class TestCartItemSerializer(TestCase):
+    fixtures = ['./fixtures/test_fixture.json']
+
+    def setUp(self):
+        self.product = Product.objects.get(id=1)
+        self.inventory = self.product.inventory
+
+    def test_validate_valid_quantity(self):
+        """When validating valid quantity nothing should happen"""
+        data = {
+            'product': self.product.id,
+            'quantity': self.inventory.quantity - 1
+        }
+        serializer = CartItemSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+
+    def test_validate_invalid_quantity(self):
+        """When validating invalid quantity ValidationError will be raised"""
+        data = {
+            'product': self.product.id,
+            'quantity': self.inventory.quantity + 1
+        }
+        serializer = CartItemSerializer(data=data)
+        with self.assertRaises(ValidationError):
+            serializer.is_valid(raise_exception=True)
+
+        self.assertIn(
+            _(f"Product {self.product.id} quantity is bigger than its stock"),
+            list(map(str, serializer.errors['non_field_errors']))
+        )
